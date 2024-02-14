201bf2ec95e6a97ce6f57f4207030e3cb8c90d14
Skip Hansen <skip@gfrn.org>
2024-02-14 10:30:53 -0800

    Add vpj files.

--- ARM_Tag_FW/OpenEPaperLink_esp32_C6_AP/update_vpj.sh
+++ ARM_Tag_FW/OpenEPaperLink_esp32_C6_AP/update_vpj.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
+VPJ_FILE=../../vs/OpenEPaperLink_esp32_C6_AP.vpj
+
 CWD=`pwd`
 (cd build/;ninja -t deps |  \
 sed -r 's!^([[:space:]]+)\.\./(.*)!\1'${CWD}/'\2!p' | \
 sed -r 's!^([[:space:]]+)([^/.])(.*)!\1'${CWD}/build/'\2\3!p' > e.d)
-dot_d_2vs.sh ~/esl/vs/OpenEPaperLink_esp32_C6_AP.vpj build
+dot_d_2vs.sh ${VPJ_FILE} build
 
--- zbs243_Tag_FW/update_vpj.sh
+++ zbs243_Tag_FW/update_vpj.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
-echo -e "\n\nBuilding 2.9 (UC8151) version..."
-make BUILD=zbs29_uc8151 CPU=8051 SOC=zbs243 > build.log
+VPJ_FILE=../vs/zbs243_Tag_FW.vpj
 
-if [ ! -e ../temp_dir ]; then mkdir ../temp_dir; fi
+echo -n "Building 2.9 (UC8151) version..."
+make BUILD=zbs29_uc8151 CPU=8051 SOC=zbs243 > build.log
+echo
+
+if [ -e ../temp_dir ]; then rm -rf ../temp_dir; fi
+mkdir ../temp_dir
 cat `find ../zbs243_shared . -name "*.d"` > ../temp_dir/e.d
-dot_d_2vs.sh ~/esl/vs/zbs243_Tag_FW.vpj ../temp_dir
+dot_d_2vs.sh ${VPJ_FILE} ../temp_dir
 
 
