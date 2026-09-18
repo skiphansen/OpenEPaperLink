cf313369023a63aad427397df767fae96d03fcab
Skip Hansen <skip@gfrn.org>
2026-09-18 06:52:53 -0700

    Moved libDrawPNG back to this repo from libDrawOwm.

---
+++ ESP32_AP-Flasher/lib/DrawPNG/include/DrawPNG.h
@@ -0,0 +1,36 @@
+#ifndef _DRAWPNG_H_
+#define _DRAWPNG_H_
+
+typedef struct {
+   PNG_OPEN_CALLBACK *pfnOpen;
+   PNG_CLOSE_CALLBACK *pfnClose;
+   PNG_READ_CALLBACK *pfnRead;
+   PNG_SEEK_CALLBACK *pfnSeek;
+} PngFileCBs_t;
+
+class DrawPNG {
+public:
+   DrawPNG(PngFileCBs_t *PngFileCBs);
+
+   bool DrawPng(String Filename,TFT_eSprite &spr);
+
+private:
+   static void *PngOpen(const char *filename, int32_t *size);
+   static void PngClose(void *handle);
+   static int32_t PngRead(PNGFILE *handle, uint8_t *buffer, int32_t length);
+   static int32_t PngSeek(PNGFILE *handle, int32_t position);
+   static int pngDrawCallback(PNGDRAW *pDraw);
+
+   int DrawCB(PNGDRAW *pDraw);
+
+   int Xoffset;
+   int Yoffset;
+   int ScalingFactor;
+   PNG png; // PNG structure (about 50K of RAM)
+   TFT_eSPI *pSpr;
+   PngFileCBs_t *pCBs;
+};
+
+#endif   // _DRAWPNG_H_
+
+
